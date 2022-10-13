fc7cc7c758ec2260a244ef9459cd22452d2cd4a3
Daniel Ephrat <dephrat@uwaterloo.ca>
2022-10-13 13:19:07 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,99 +1,108 @@
 import torch
 from torch import nn
 from torch.utils.data import DataLoader
+import torchvision
 from torchvision import transforms, datasets
-#from torchvision.transforms import ToTensor
+import torch.nn.functional as F
+import numpy as np
+import matplotlib.pyplot as plt
 
 data_transform = transforms.Compose([
-        #transforms.RandomCrop(80),
         transforms.Resize((224,224)),
         transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406],
                              std=[0.229, 0.224, 0.225])
     ])
-train_dataset = datasets.ImageFolder(root='train_dataset',
+
+train_dataset = datasets.ImageFolder(root='data/train_dataset',
                                            transform=data_transform)
 train_dataset_loader = torch.utils.data.DataLoader(train_dataset,
                                              batch_size=4, shuffle=True,
                                              num_workers=4)
-
-test_dataset = datasets.ImageFolder(root='test_dataset',
-                                       transform=data_transform)
-test_dataset_loader = torch.utils.data.DataLoader(test_dataset,
-                                                 batch_size=4, shuffle=True,
-                                                  num_workers=4)
 
 # Get cpu or gpu device for training.
 device = "cuda" if torch.cuda.is_available() else "cpu"
 print(f"Using {device} device")
 
 # Define model
-class NeuralNetwork(nn.Module):
+class ConvNeuralNetwork(nn.Module):
     def __init__(self):
         super().__init__()
-        self.flatten = nn.Flatten()
-        self.linear_relu_stack = nn.Sequential(
-            #nn.Linear(28*28, 512),
-            nn.Linear(150528, 512),
-            nn.ReLU(),
-            nn.Linear(512, 512),
-            nn.ReLU(),
-            nn.Linear(512, 10)
-        )
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        #self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc1 = nn.Linear(44944, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
 
     def forward(self, x):
-        x = self.flatten(x)
-        logits = self.linear_relu_stack(x)
-        return logits
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = torch.flatten(x, 1) # flatten all dimensions except batch
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
 
-model = NeuralNetwork().to("cpu")
+model = ConvNeuralNetwork().to(device)
 print(model)
 
 # Loss function and optimizer 
-loss_fn = nn.CrossEntropyLoss()
-optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
+#loss_fn = nn.CrossEntropyLoss()
+#optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
 
-# Train and test code
-def train(dataloader, model, loss_fn, optimizer):
-    size = len(dataloader.dataset)
-    model.train()
-    for batch, (X, y) in enumerate(dataloader):
-    #    X, y = X.to(device), y.to(device)
 
-        # Compute prediction error
-        pred = model(X)
-        loss = loss_fn(pred, y)
+def train(dataset_loader, model, criterion, optimizer, epochs):
+    for epoch in range(epochs):  # loop over the dataset multiple times
 
-        # Backpropagation
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+        running_loss = 0.0
+        for i, data in enumerate(dataset_loader, 0):
+            # get the inputs; data is a list of [inputs, labels]
+            inputs, labels = data
 
-        if batch % 100 == 0:
-            loss, current = loss.item(), batch * len(X)
-            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
-            
-def test(dataloader, model, loss_fn):
-    size = len(dataloader.dataset)
-    num_batches = len(dataloader)
-    model.eval()
-    test_loss, correct = 0, 0
-    with torch.no_grad():
-        for X, y in dataloader:
-            X, y = X.to(device), y.to(device)
-            pred = model(X)
-            test_loss += loss_fn(pred, y).item()
-            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
-    test_loss /= num_batches
-    correct /= size
-    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+            # zero the parameter gradients
+            optimizer.zero_grad()
+
+            # forward + backward + optimize
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            # print statistics
+            running_loss += loss.item()
+            if i % 2000 == 1999:    # print every 2000 mini-batches
+                print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
+                running_loss = 0.0
+
+    print('Finished Training')
     
-    
-# Time to actually test the data
-epochs = 5
-for t in range(epochs):
-    print(f"Epoch {t+1}\n-------------------------------")
-    train(train_dataset_loader, model, loss_fn, optimizer)
-    test(test_dataset_loader, model, loss_fn)
-print("Done!")
+train(train_dataset_loader, model, criterion, optimizer, 5)
+
+
+
+test_dataset = datasets.ImageFolder(root='data/test_dataset',
+                                       transform=data_transform)
+test_dataset_loader = torch.utils.data.DataLoader(test_dataset,
+                                                 batch_size=4, shuffle=True,
+                                                  num_workers=4)
+
+correct = 0
+total = 0
+# since we're not training, we don't need to calculate the gradients for our outputs
+with torch.no_grad():
+    for data in test_dataset_loader:     
+        images, labels = data
+        # calculate outputs by running images through the network
+        outputs = model(images)
+        # the class with the highest energy is what we choose as prediction
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+print(f'Accuracy of the network on the test images: {100 * correct / total} %')
+print(hash_table)
